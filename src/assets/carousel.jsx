7aedf529770a9bf10848d15afe217a9799f0f318
Naki <leandroreisubi@gmail.com>
2024-12-27 15:23:10 -0300

    home page images

---
+++ src/assets/carousel.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Slider from "react-slick";
+import CT1 from "../assets/images/CT1.jpg";
+import CT2 from "../assets/images/CT2.jpg";
+import CT3 from "../assets/images/CT3.jpg";
+import CT4 from "../assets/images/CT4.jpg";
+
+const TravelCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const images = [
+    { src: CT1, alt: "Image 1" },
+    { src: CT2, alt: "Image 2" },
+    { src: CT3, alt: "Image 3" },
+    { src: CT4, alt: "Image 4" },
+  ];
+
+  return (
+    <div className="w-full mx-auto max-w-full px-4">
+      <Slider {...settings}>
+        {images.map((img, index) => (
+          <div key={index}>
+            <img
+              src={img.src}
+              alt={`Slide ${index + 1}`}
+              style={{ height: "550px" }}
+              className="w-full object-cover rounded-lg"
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default TravelCarousel;
--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="bg-blue-500 p-4"></nav>;
+  return (
+    <nav className="bg-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <h1 className="text-blue-600 text-2xl font-poppins">AKS</h1>
+          <p className="text-blue-500 text-2xl mt-1 font-poppins">TUR</p>
+        </div>
+        <ul className="flex space-x-4">
+          <li className="text-orange-400 hover:underline">Home</li>
+          <li className="text-orange-400 hover:underline">Destinos</li>
+          <li className="text-orange-400 hover:underline">Serviços</li>
+          <li className="text-orange-400 hover:underline">Contato</li>
+          <li className="text-orange-400 hover:underline">Social</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
-export default navbar;
+export default Navbar;
