774a8c753860449d501331ade7425bb1710216d7
Naki <leandroreisubi@gmail.com>
2025-04-16 10:40:41 -0300

    Navbar retrabalhado

--- src/App.js
+++ src/App.js
@@ -31,13 +31,13 @@ function App() {
 
   return (
     <div className=" ">
-      <Navbar />
       <TravelCarousel />
       {/* 
       <div className="container mx-auto">
         <FlightsForm />
       </div>
       */}
+      <Navbar />
       <div className="container mx-auto">
         <CardsBar />
       </div>
