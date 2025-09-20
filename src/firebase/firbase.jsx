e42ef3a1a13444439a7222c7073497133beb017e
naki <leandroreisubi@gmail.com>
2025-09-20 17:14:25 -0300

    Correção

--- src/firebase/firbase.jsx
+++ src/firebase/firbase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAT4JAO1Iy_XjY6zIG3wHalofKIdbORtmk",
-  authDomain: "aks-tour.firebaseapp.com",
-  projectId: "aks-tour",
-  storageBucket: "aks-tour.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:0987654321",
+  apiKey: AIzaSyAT4JAO1Iy_XjY6zIG3wHalofKIdbORtmk,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
