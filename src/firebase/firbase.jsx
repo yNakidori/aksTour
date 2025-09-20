e7d88a94359953140a9d026d7883035711760df4
naki <leandroreisubi@gmail.com>
2025-09-20 17:16:54 -0300

    Correção

--- src/firebase/firbase.jsx
+++ src/firebase/firbase.jsx
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: AIzaSyAT4JAO1Iy_XjY6zIG3wHalofKIdbORtmk,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
